a7d96fe0708e3efc210379cca2f374d9145846c9
Develop Bencosme Diaz <benjaminbencosme237@gmail.com>
2023-11-14 16:46:46 -0400

    Configuracion del Proyecto

---
+++ SistemaI.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaI.Modelos;
+using System.Reflection;
+
+namespace SistemaI.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Bodega> Bodega { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+    }
+    
+}
---
+++ SistemaI.Modelos/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaI.Modelos.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
